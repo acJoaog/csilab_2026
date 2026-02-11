d9dc2fa087c9b58beec4eb17ae86b29eaf7cd6eb
acJoaog <joaog.azc@gmail.com>
2026-02-11 17:40:46 -0300

    postgres paths fix

--- postgres/test/test-postgres-connection.sh
+++ postgres/test/test-postgres-connection.sh
@@ -6,7 +6,7 @@ chmod 600 ../../certs/client.key
 
 # Teste com SSL obrigatório
 echo "1. Testando conexão com SSL (deve funcionar):"
-PGPASSWORD=admin psql "host=localhost port=5432 dbname=smartlab_db user=admin sslmode=require sslcert=../../certs/client.crt sslkey=../../certs/client.key" -c "SELECT 'Conexão SSL bem-sucedida!' as status;"
+PGPASSWORD=admin psql "host=localhost port=5432 dbname=smartlab_db user=admin sslmode=require sslcert=../../export/client.crt sslkey=../../export/client.key" -c "SELECT 'Conexão SSL bem-sucedida!' as status;"
 
 echo ""
 echo "2. Testando conexão sem SSL (deve falhar):"
@@ -14,6 +14,6 @@ PGPASSWORD=admin psql "host=localhost port=5432 dbname=smartlab_db user=admin ss
 
 echo ""
 echo "3. Verificando status do SSL no PostgreSQL:"
-PGPASSWORD=admin psql "host=localhost port=5432 dbname=smartlab_db user=admin sslmode=require sslcert=../../certs/client.crt sslkey=../../certs/client.key" -c "SHOW ssl; SELECT version();"
+PGPASSWORD=admin psql "host=localhost port=5432 dbname=smartlab_db user=admin sslmode=require sslcert=../../export/client.crt sslkey=../../export/client.key" -c "SHOW ssl; SELECT version();"
 
-chmod 777 ../../certs/client.key
+chmod 777 ../../export/client.key
